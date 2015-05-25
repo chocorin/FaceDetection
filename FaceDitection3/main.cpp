dfedd4e352ab979346e4044a7f94ab1427688b8a
chocorin <m236029r@mail.kyutech.jp>
2015-05-25 12:19:01 +0900

    20150525_矩形での描写

--- FaceDitection3/main.cpp
+++ FaceDitection3/main.cpp
@@ -27,9 +27,6 @@ RNG rng(12345);
 Mat frame;
 
 
-// commit test
-//ここが変化していたら大丈夫
-
 /**
  * @function main
  */
@@ -37,7 +34,8 @@ int main( int argc, char* argv[] )
 {
 	//入力画像の読み込み　引数がなければサンプル画像を読み込む
 	if (argc < 2) {
-		frame = imread("P1120221.JPG", CV_LOAD_IMAGE_COLOR);
+		//frame = imread("P1120221.JPG", CV_LOAD_IMAGE_COLOR);
+		frame = imread("obama_1_0008.bmp", CV_LOAD_IMAGE_COLOR);
 	}
 	else {
 		frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
@@ -92,7 +90,7 @@ void detectAndDisplay( Mat frame )
 
    //-- Detect faces
    //正面顔検出//
-   face_cascade.detectMultiScale( frame_gray, faces, 1.11, 1, 0|CV_HAAR_SCALE_IMAGE, Size(20, 20) );
+   face_cascade.detectMultiScale( frame_gray, faces, 1.11, 4, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
    //http://docs.opencv.org/modules/objdetect/doc/cascade_classification.html
    //detectMultiScale(const Mat& image, vector<Rect>& objects, double scaleFactor=1.1, int minNeighbors=3, int flags=0, Size minSize=Size(), Size maxSize=Size())
@@ -105,8 +103,42 @@ void detectAndDisplay( Mat frame )
 	  //Point  x と y によって指定される 2 次元の点を表現するクラス
       Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
 	  //楕円の描画
-      ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 1, 4, 0 );
+      //ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 1, 4, 0 );
 	  
+	  //矩形の描画
+	  //個々の検出領域を表す変数
+	  vector<Rect>::const_iterator r;
+      //検出領域の一つ一つに対するループ処理
+	  for( r = faces.begin();  r != faces.end(); r++ ) {
+
+		Point pt1; //領域の始点
+		Point pt2; //領域の終点
+
+      //得られた結果をもとに、領域の始点と終点を計算
+		pt1.x = r->x;
+		pt1.y = r->y;
+		pt2.x = r->x + r->width - 1;
+		pt2.y = r->y + r->height - 1;
+
+      //検出した領域にあわせて矩形を描く
+		rectangle( frame, pt1, pt2, CV_RGB(255, 0, 255), 1, 4, 0 );
+
+		//矩形描写座標の取得
+		//if( i = faces.size() - 1) {
+			printf("i=%d;\n", i);
+			int pt1_x = pt1.x;	
+			printf("pt1_x=%d;\n",pt1_x);
+			int pt1_y = pt1.y;	
+			printf("pt1_y=%d;\n",pt1_y);
+			int pt2_x = pt2.x;	
+			printf("pt2_x=%d;\n",pt2_x);
+			int pt2_y = pt2.y;	
+			printf("pt2_y=%d;\n",pt2_y);
+			//obama_1_0008.bmp出力結果
+			//Point(209, 100) Point(263, 154);d
+		//}	
+	  }
+
 	  //数値から文字列に変換
 	  //std::to_string
 	  string num_str = to_string(i+1);
@@ -116,6 +148,11 @@ void detectAndDisplay( Mat frame )
 
 	  //番号の描画
 	  putText( frame, num_str, number, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 1, 4, false);
+
+	  //矩形描画てすと
+	  //rectangle( frame, Point(861, 160), Point(898, 197), Scalar(255, 0, 0), 1, 4, 0);
+
+
    }
 
 	
@@ -130,17 +167,17 @@ void detectAndDisplay( Mat frame )
 
    for( size_t j = 0; j < profiles.size(); j++ ) {
       Point profile_center( profiles[j].x + profiles[j].width/2, profiles[j].y + profiles[j].height/2 );
-	  ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 1, 4, 0 );
+	  //ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 1, 4, 0 );
 
 	   //数値から文字列に変換
 	  //std::to_string
 	  string num_str = to_string(j+1);
 
 	  //番号の描画する場所
-	 Point profile_number( profiles[j].x + profiles[j].width/2 + 20, profiles[j].y + profiles[j].height/2 - 30);
+	 //Point profile_number( profiles[j].x + profiles[j].width/2 + 20, profiles[j].y + profiles[j].height/2 - 30);
 
 	  //番号の描画
-	  putText( frame, num_str, profile_number, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 0), 1, 4, false);
+	  //putText( frame, num_str, profile_number, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 0), 1, 4, false);
    }
  
    //-- Show what you got
