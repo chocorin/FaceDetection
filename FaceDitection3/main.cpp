c5cae8688d66eaf8d4a7716b4966ce3fc36fb5cc
chocorin <m236029r@mail.kyutech.jp>
2015-05-26 19:14:23 +0900

    動画の読み込み（引数未対応）

--- FaceDitection3/main.cpp
+++ FaceDitection3/main.cpp
@@ -24,7 +24,7 @@ CascadeClassifier profiles_cascade;
 string window_name = "Capture - Face detection";
 RNG rng(12345);
 
-Mat frame;
+//Mat frame;
 
 
 /**
@@ -32,34 +32,61 @@ Mat frame;
  */
 int main( int argc, char* argv[] )
 {
+	//入力動画の読み込み
+	VideoCapture cap("spe_2010_0127_obama_5.avi");
+
+	//読み込みがきちんとできているかのチェック
+	if(!cap.isOpened())
+		return -1;
+
+	Mat frame;
+
+
 	//入力画像の読み込み　引数がなければサンプル画像を読み込む
-	if (argc < 2) {
+	//if (argc < 2) {
 		//frame = imread("P1120221.JPG", CV_LOAD_IMAGE_COLOR);
-		frame = imread("obama_1_0052.bmp", CV_LOAD_IMAGE_COLOR);
-	}
-	else {
-		frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	}
+		//frame = imread("obama_1_0052.bmp", CV_LOAD_IMAGE_COLOR);
+	//}
+	//else {
+	//	frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	//}
 
     //画像が開けなければエラー表示
-	if (frame.empty()) {
-		printf("Cannot open image\n");
-		exit (-1);
-	}
+	//if (frame.empty()) {
+		//printf("Cannot open image\n");
+		//exit (-1);
+	//}
 
 
     //-- 1. Load the cascades
     if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
     if( !profiles_cascade.load( profile_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
+	for(;;) {
+    // （3）動画ファイルから1フレーム分の画像データを取得して、変数frameに格納する
+    cap >> frame;
+
+    // 画像データ取得に失敗したらループを抜ける
+    if (!frame.empty()) {
+		//detectAndDisplay( frame );
+		//imshow("window", frame);
+	}
+	else
+		break;
+
+    // 取得した画像データをウィンドウ表示する
+    
+    if(waitKey(30) >= 0) break;
+    }
+	return 0;
  
     //-- 2. Apply the classifier to the frame
-    if( !frame.empty() ) { 
-		detectAndDisplay( frame ); 
-		//imshow("window", frame);
-		destroyAllWindows();
-	}
-    else { printf(" --(!) No captured frame -- Break!"); }
+    //if( !frame.empty() ) { 
+		//detectAndDisplay( frame ); 
+		////imshow("window", frame);
+		//destroyAllWindows();
+	//}
+    //else { printf(" --(!) No captured frame -- Break!"); }
 
 	return 0;
  }
@@ -90,7 +117,7 @@ void detectAndDisplay( Mat frame )
 
    //-- Detect faces
    //正面顔検出//
-   face_cascade.detectMultiScale( frame_gray, faces, 1.200191, 0, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50) );
+   face_cascade.detectMultiScale( frame_gray, faces, 1.1, 4, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50) );
 
    //http://docs.opencv.org/modules/objdetect/doc/cascade_classification.html
    //detectMultiScale(const Mat& image, vector<Rect>& objects, double scaleFactor=1.1, int minNeighbors=3, int flags=0, Size minSize=Size(), Size maxSize=Size())
@@ -164,7 +191,7 @@ void detectAndDisplay( Mat frame )
 			//2番目に検出
 			//Point(209, 118), Point(258, 167)
 			//Size(49, 49)
-			//縮小量を1.200192に変更すると1番目に検出可能
+			//縮小量を1.200192とか1.2001918に変更すると1番目に検出可能
 			//この条件では obama_1_0022.bmp は検出不可、縮小量を1.11に変更することで検出可能
 		//}	
 	
@@ -177,7 +204,7 @@ void detectAndDisplay( Mat frame )
    std::vector<Rect> profiles;
 
    //-- In each face, detect profiles
-   profiles_cascade.detectMultiScale( faceROI, profiles, 1.05, 0, 0 |CV_HAAR_SCALE_IMAGE, Size(40, 40) );
+   profiles_cascade.detectMultiScale( faceROI, profiles, 1.1, 3, 0 |CV_HAAR_SCALE_IMAGE, Size(40, 40) );
 
    for( size_t j = 0; j < profiles.size(); j++ ) {
       Point profile_center( profiles[j].x + profiles[j].width/2, profiles[j].y + profiles[j].height/2 );
@@ -196,5 +223,5 @@ void detectAndDisplay( Mat frame )
  
    //-- Show what you got
    imshow( window_name, frame );
-   waitKey(0);
+   //waitKey(0);
 }
