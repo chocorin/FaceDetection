fcec71bf0a45f700a2cbabeaa886d56ee9e607a2
chocorin <m236029r@mail.kyutech.jp>
2015-06-02 15:42:17 +0900

    正面顔矩形描画にポインタを使っていたのを修正

--- FaceDitection3/main.cpp
+++ FaceDitection3/main.cpp
@@ -52,7 +52,7 @@ int main( int argc, char* argv[] )
 		org_filename = argv[1];
 		char *name, *extention;
 
-	    /* 1回目の呼出し */
+		/* 1回目の呼出し */
 		name = strtok(argv[1], ".");
 
 		/* 2回目以降の呼出し */
@@ -120,61 +120,53 @@ int main( int argc, char* argv[] )
 	//}
 
 	return 0;
- }
+	}
 
 
-/**
- * @function detectAndDisplay
- */
-void detectAndDisplay( Mat frame )
-{
-   std::vector<Rect> faces;
-   //std::vector<型> オブジェクト名
+	/**
+	* @function detectAndDisplay
+	*/
+	void detectAndDisplay( Mat frame )
+	{
+	std::vector<Rect> faces;
+	//std::vector<型> オブジェクト名
 
-   Mat frame_gray;
+	Mat frame_gray;
 
 	
 
-   cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-   //画像の色空間の変換
-   //カラー画像からグレースケール画像に変換している
-   //void cvtColor(const Mat, Mat, int code(色空間の変換コード), int dstCn=0(出力画像のチャネル数、0の場合は自動的に求められる) )
+	cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+	//画像の色空間の変換
+	//カラー画像からグレースケール画像に変換している
+	//void cvtColor(const Mat, Mat, int code(色空間の変換コード), int dstCn=0(出力画像のチャネル数、0の場合は自動的に求められる) )
 
-   equalizeHist( frame_gray, frame_gray );
-   //ヒストグラムの均一化
-   //void equalizeHist(const Mat, Mat)
-   //均一化を行うことでより鮮明な画像になる
+	equalizeHist( frame_gray, frame_gray );
+	//ヒストグラムの均一化
+	//void equalizeHist(const Mat, Mat)
+	//均一化を行うことでより鮮明な画像になる
 
 
-   //-- Detect faces
-   //正面顔検出//
-   face_cascade.detectMultiScale( frame_gray, faces, 1.1, 4, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50) );
+	//-- Detect faces
+	//正面顔検出//
+	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 4, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50) );
 
-   //http://docs.opencv.org/modules/objdetect/doc/cascade_classification.html
-   //detectMultiScale(const Mat& image, vector<Rect>& objects, double scaleFactor=1.1, int minNeighbors=3, int flags=0, Size minSize=Size(), Size maxSize=Size())
-   //scaleFactor –各画像スケールにおける縮小量
-   //minNeighbors 最低矩形数
+	//http://docs.opencv.org/modules/objdetect/doc/cascade_classification.html
+	//detectMultiScale(const Mat& image, vector<Rect>& objects, double scaleFactor=1.1, int minNeighbors=3, int flags=0, Size minSize=Size(), Size maxSize=Size())
+	//scaleFactor –各画像スケールにおける縮小量
+	//minNeighbors 最低矩形数
 
    
-   for( size_t i = 0; i < faces.size(); i++ ) {
- 	  //Point  x と y によって指定される 2 次元の点を表現するクラス
-      Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
- 	  //楕円の描画
-      //ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 1, 8, 0 );
- 	  
-	  //矩形の描画
-	  //個々の検出領域を表す変数
-	  vector<Rect>::const_iterator r;
-      //検出領域の一つ一つに対するループ処理
-	  for( r = faces.begin();  r != faces.end(); r++ ) {
-		Point pt1; //領域の始点
-		Point pt2; //領域の終点
+	for( size_t i = 0; i < faces.size(); i++ ) {
+		Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
 
-		//得られた結果をもとに、領域の始点と終点を計算
-		pt1.x = r->x;
-		pt1.y = r->y;
-		pt2.x = r->x + r->width - 1;
-		pt2.y = r->y + r->height - 1;
+		Point pt1;
+		Point pt2;
+
+		pt1.x = faces[i].x;
+		pt1.y = faces[i].y;
+		pt2.x = faces[i].x + faces[i].width - 1;
+		pt2.y = faces[i].y +faces[i].height - 1;
+
 
 		//中心座標の算出
 		Point cpt;
@@ -194,21 +186,21 @@ void detectAndDisplay( Mat frame )
 		rectangle( frame, pt1, pt2, CV_RGB(255, 255, 255), 3, 8, 0 );
 
 		rectangle( frame, pt1, pt2, CV_RGB(255, 0, 255), 1, 8, 0 );
-       }
 
- 	  //数値から文字列に変換
- 	  //std::to_string
- 	  string num_str = to_string(i+1);
- 
- 	  //番号の描画する場所
- 	  Point number( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 - 60 );
- 
- 	  //番号の描画
- 	  putText( frame, num_str, number, FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0, 0, 255), 2, 8, 0);
 
-	  //矩形描画てすと
-	  //rectangle( frame, Point(201, 93), Point(276, 168), Scalar(255, 0, 0), 1, 8, 0);
-    }
+		//数値から文字列に変換
+		//std::to_string
+		string num_str = to_string(i+1);
+ 
+		//番号の描画する場所
+		Point number( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 - 60 );
+ 
+		//番号の描画
+		putText( frame, num_str, number, FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0, 0, 255), 2, 8, 0);
+
+		//矩形描画てすと
+		//rectangle( frame, Point(201, 93), Point(276, 168), Scalar(255, 0, 0), 1, 8, 0);
+		}
 
   
 
@@ -244,34 +236,34 @@ void detectAndDisplay( Mat frame )
 
 	
 
-   //横顔検出//
-   Mat faceROI = frame_gray;
+	//横顔検出//
+	Mat faceROI = frame_gray;
 
-   std::vector<Rect> profiles;
+	std::vector<Rect> profiles;
 
-   //-- In each face, detect profiles
-   profiles_cascade.detectMultiScale( faceROI, profiles, 1.1, 3, 0 |CV_HAAR_SCALE_IMAGE, Size(40, 40) );
+	//-- In each face, detect profiles
+	profiles_cascade.detectMultiScale( faceROI, profiles, 1.1, 3, 0 |CV_HAAR_SCALE_IMAGE, Size(40, 40) );
 
-   for( size_t j = 0; j < profiles.size(); j++ ) {
-      Point profile_center( profiles[j].x + profiles[j].width/2, profiles[j].y + profiles[j].height/2 );
+	for( size_t j = 0; j < profiles.size(); j++ ) {
+		Point profile_center( profiles[j].x + profiles[j].width/2, profiles[j].y + profiles[j].height/2 );
 
-	  //楕円の描画
-	  ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 5, 8, 0 );
-	  ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 255, 255 ), 3, 8, 0 );
-	  ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 1, 8, 0 );
+		//楕円の描画
+		ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 5, 8, 0 );
+		ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 255, 255 ), 3, 8, 0 );
+		ellipse( frame, profile_center, Size( profiles[j].width/2, profiles[j].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 1, 8, 0 );
 
-	   //数値から文字列に変換
-	  //std::to_string
-	  string num_str = to_string(j+1);
+		//数値から文字列に変換
+		//std::to_string
+		string num_str = to_string(j+1);
 
-	  //番号の描画する場所
-	 Point profile_number( profiles[j].x + profiles[j].width/2 + 20, profiles[j].y + profiles[j].height/2 - 60);
+		//番号の描画する場所
+		Point profile_number( profiles[j].x + profiles[j].width/2 + 20, profiles[j].y + profiles[j].height/2 - 60);
 
-	  //番号の描画
-	  putText( frame, num_str, profile_number, FONT_HERSHEY_SIMPLEX, 0.8, Scalar(255, 255, 0), 2, 8, 0);
-   }
+		//番号の描画
+		putText( frame, num_str, profile_number, FONT_HERSHEY_SIMPLEX, 0.8, Scalar(255, 255, 0), 2, 8, 0);
+	}
  
-   //-- Show what you got
-   //imshow( window_name, frame );
-   //waitKey(0);
+	//-- Show what you got
+	//imshow( window_name, frame );
+	//waitKey(0);
 }
